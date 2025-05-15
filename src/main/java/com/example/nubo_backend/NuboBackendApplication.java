691b61931052b7256bbe00ee03b8394daed503e3
[Cloud3] 원다빈 <ynibad@gmail.com>
2025-05-15 19:02:02 +0900

    Initial commit: 프로젝트 초기 설정

---
+++ src/main/java/com/example/nubo_backend/NuboBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nubo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuboBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuboBackendApplication.class, args);
+	}
+
+}
