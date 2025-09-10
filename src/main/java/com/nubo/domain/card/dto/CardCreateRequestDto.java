8dc183d65fcfb1dbf92ba8a2f2d5e66c2509737b
[Cloud3] 원다빈 <ynibad@gmail.com>
2025-09-10 14:16:49 +0900

    refactor: 카드 생성 요청 dto 플랫폼 컬럼 삭제

--- src/main/java/com/nubo/domain/card/dto/CardCreateRequestDto.java
+++ src/main/java/com/nubo/domain/card/dto/CardCreateRequestDto.java
@@ -1,6 +1,5 @@
 package com.nubo.domain.card.dto;
 
-import com.nubo.domain.video.type.Platform;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,6 @@ public class CardCreateRequestDto {
 
   // video 기본 정보
   private String videoUrl;
-  private Platform platform;
 
   // 사용자 지정 보드 (optional)
   private List<Long> boardIds;
