2ed30ecfc2aeb770e3ed3ad34e2f0ce09b90ea11
Dabin Won <ynibad@gmail.com>
2025-11-21 20:15:40 +0900

    chore: api 요청 테스트용 추천 api 엔드포인트 전체접근 임시 허용

--- src/main/java/com/nubo/global/config/SecurityConfig.java
+++ src/main/java/com/nubo/global/config/SecurityConfig.java
@@ -26,7 +26,8 @@ public class SecurityConfig {
       .csrf(csrf -> csrf.disable())
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/", "/api/auth/**", "/favicon.ico").permitAll()
+        .requestMatchers("/", "/api/auth/**", "/favicon.ico", "/api/home/recommendation/**")
+        .permitAll()
         .anyRequest().authenticated()
       )
       .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
